9f9e615c3d3af5b7b97199ad373e5c78612f653a
Azad Kshitij <azadkshitij08302001@gmail.com>
2020-08-20 22:34:33 +0530

    Add a commit message

--- Assets/Scripts/Player/Player.gd
+++ Assets/Scripts/Player/Player.gd
@@ -15,6 +15,8 @@ var state = MOVE
 
 # variables
 var speed := Vector2.ZERO
+var input_vector := Vector2.ZERO
+var roll_vector := Vector2.RIGHT
 
 # Onready Variables
 onready var animationPlayer = $AnimationPlayer 
@@ -25,32 +27,23 @@ onready var animationState = animationTree.get("parameters/playback")
 func _ready():
 	animationTree.active = true
 
-
 func _process(delta):
+	input_vector = get_input()
+	
+
+func _physics_process(delta):
 	match state:
 		MOVE:
 			move_player(delta)
 		ROLL:
-			roll(delta)
+			roll()
 		ATTACK:
-			attack(delta)
-			
-	
+			attack()
 	
 	
 func move_player(delta):
-	var input_vector := Vector2.ZERO
-	if input_vector.y == 0:
-		input_vector.x =  Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left") 
-	if input_vector.x == 0:
-		input_vector.y =  Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	input_vector = input_vector.normalized()
-	
 	if input_vector != Vector2.ZERO:
-		animationTree.set("parameters/Run/blend_position", input_vector)
-		animationTree.set("parameters/Idle/blend_position", input_vector)
-		animationTree.set("parameters/Attack/blend_position", input_vector)
-		animationState.travel("Run")
+		roll_vector = input_vector
 		speed = speed.move_toward(input_vector*MAXSPEED,ACCELARATION * delta)
 	else:
 		animationState.travel("Idle")
@@ -59,14 +52,47 @@ func move_player(delta):
 	speed = move_and_slide(speed)
 	
 	
+func get_input():
+	# Move Input
+	if input_vector.y == 0:
+		input_vector.x =  Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left") 
+	if input_vector.x == 0:
+		input_vector.y =  Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	# Attack Input
 	if Input.is_action_pressed("ak_attack"):
 		state = ATTACK
+	if Input.is_action_pressed("ak_roll"):
+		state = ROLL
+		
+	return input_vector
 
-func attack(delta):
+func attack():
 	animationState.travel("Attack")
 
-func roll(delta):
-	pass
+func roll():
+	speed = move_and_slide(roll_vector * MAXSPEED)
+	animationState.travel("Roll")
 	
 func change_state(num):
 	state = num
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
