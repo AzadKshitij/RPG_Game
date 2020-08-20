6d86a3ce3c19d4a20c63020f0468b76078b848d1
Azad Kshitij <azadkshitij08302001@gmail.com>
2020-08-20 11:52:22 +0530

    seocnd

--- Assets/Scripts/Player/Player.gd
+++ Assets/Scripts/Player/Player.gd
@@ -1,28 +1,35 @@
 extends KinematicBody2D
 
+# constants
+const MAXSPEED = 80
+const ACCELARATION = 500 
+const FRICTION = 500
+
+# variables
 var speed := Vector2.ZERO
 
-func _ready():
-	pass
+# Onready Variables
+onready var animationPlayer = $AnimationPlayer 
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 func _process(delta):
-	get_speed()
-	move_and_collide(speed * delta)
+	move_player(delta)
 	
-func get_speed():
+func move_player(delta):
 	var input_vector := Vector2.ZERO
-	input_vector.x =  Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left") 
-	input_vector.y =  Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	if Input.is_key_pressed(KEY_D):
-		input_vector.x = 1
-	if Input.is_key_pressed(KEY_A):
-		input_vector.x = -1
-	if Input.is_key_pressed(KEY_W):
-		input_vector.y = -1
-	if Input.is_key_pressed(KEY_S):
-		input_vector.y = 1
+	if input_vector.y == 0:
+		input_vector.x =  Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left") 
+	if input_vector.x == 0:
+		input_vector.y =  Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
 	if input_vector != Vector2.ZERO:
-		speed = input_vector * 40
-	else: speed = Vector2.ZERO
-	
-	
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationState.travel("Run")
+		speed = speed.move_toward(input_vector*MAXSPEED,ACCELARATION * delta)
+	else:
+		animationState.travel("Idle")
+		speed = speed.move_toward(Vector2.ZERO,FRICTION * delta)
+	speed = move_and_slide(speed)
